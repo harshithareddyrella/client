aa2d2cea55b0868e7e850bc877575d134e15d054
harshithareddyrella <harshithareddyrella@gmail.com>
2021-07-08 16:00:41 +0530

    only meeting page with updated npm

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import firebase from "firebase/app";
+import Meeting from './Meeting';
+// import { ContextProvider } from './SocketContext';
+import './styles.css';
+
+
+ReactDOM.render(
+    
+        <Meeting />,
+document.getElementById('root'));
